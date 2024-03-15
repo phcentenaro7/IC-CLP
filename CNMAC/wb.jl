3b7ec34dfead142691cafb8129a081bdc6ae1c17
Pedro Centenaro <phc7blu@gmail.com>
2024-03-15 15:01:05 -0300

    corrected a mistake in the placements of the wall-building algorithms for CNMAC and the remainder of the project

--- CNMAC/wb.jl
+++ CNMAC/wb.jl
@@ -12,102 +12,50 @@ end
 
 new_id(table) = size(table, 1) + 1
 
-"""
-Creates a new table of containers. Each container is defined by:
-
-* Width, height and depth;
-* A variable indicating the depth that remains unused by any layers.
-"""
 function new_container_table()
     container_table = DataFrame(id=Int[], width=Real[], height=Real[], depth=Real[], remaining_depth=Real[])
     return container_table
 end
 
-"""
-Creates a new table of layers. Each layer is defined by:
-
-* The container to which it belongs;
-* The item that was used to determine the layer's depth;
-* The ``z`` coordinate of the layer within the container;
-* The depth of the layer.
-"""
 function new_layer_table()
     layer_table = DataFrame(id=Int[], container=Int[], item=Int[], z=Real[], depth=Real[])
     return layer_table
 end
 
-"""
-Creates a new table of spaces. Each space is defined by:
-
-* The layer to which it belongs;
-* Width, height and depth;
-* Coordinates defining its position within the layer;
-
-There is also a number of other characteristics needed to describe a space. The first is its `type`. If the space was created at the same time as the layer, then it's a `primary` space. Otherwise, it's either a `widthwise`, `heightwise` or `depthwise` space.
-
-A space is also described by its `status`. Initially, all spaces are marked as `new`. If the heuristic is able to fill a space (partially or completely), the space is marked as `filled`. Otherwise, the space is `rejected`. A space that was `rejected` may later turn into an `amalgamated` space, if it is used to create an amalgamation with a space in the next layer.
-
-If a space is amalgamated with another, the other space's ID is stored as a `partner`. Furthermore, when a space in the *current layer* is amalgamated, the space it originated from is marked as a `sibling`, and vice versa. The flexible width between these spaces is also stored in this table. Finally, if a `widthwise`, `heightwise` or `depthwise` originates from an amalgam, this is also registered in the table.
-"""
 function new_space_table()
     space_table = DataFrame(id=Int[], layer=Int[], width=Real[], height=Real[], depth=Real[], x=Real[], y=Real[], z=Real[], type=Symbol[], status=Symbol[], partner=Int[], sibling=Int[], flexible_width=Real[], from_amalgam=Bool[])
     return space_table
 end
 
-"""
-Creates a new table of items. Each item is defined by:
-
-* The dimensions of the item;
-* The quantity initially in stock;
-* Whether the item is open or not (that is, whether any item of this type has already been placed or not).
-"""
 function new_item_table()
     item_table = DataFrame(id=Int[], dim1=Real[], dim2=Real[], dim3=Real[], quantity=Int[], open=Bool[])
     return item_table
 end
 
-"""
-Creates a new table of placements. Each placement is defined by:
-
-* The space its positions are relative to;
-* The item type of the placement;
-* The quantity of items stacked in this placement column;
-* The width, height and depth of each unit of this item type;
-* The x, y and z coordinates of the placement, relative to its space.
-"""
 function new_placement_table()
     placement_table = DataFrame(space=Int[], item=Int[], quantity=Real[], width=Real[], height=Real[], depth=Real[], x=[], y=[], z=[])
     return placement_table
 end
 
-"""
-Adds a container to the database.
-"""
 function add_container!(db::Database, dims::Vector{<:Real})
     id = new_id(db.containers)
     push!(db.containers, [id, dims..., dims[3]])
     return db.containers[id,:]
 end
 
-"""
-Adds a layer to the database.
-"""
 function add_layer!(db::Database, container, item, z, depth)
     id = new_id(db.layers)
     push!(db.layers, [id, container, item, z, depth])
     return db.layers[id,:]
 end
 
-"""
-Adds a space to the database.
-"""
 function add_space!(db::Database, layer, dims, coords, type)
     id = new_id(db.spaces)
     push!(db.spaces, [id, layer, dims..., coords..., type, :new, 0, 0, 0, false])
     return db.spaces[id,:]
 end
 
-function create_new_layer!(db::Database, container_id)
+function create_new_layer(db::Database)
     item_id = select_primary_item(db)
     layer_depth = db.items[item_id, [:dim1,:dim2,:dim3]] |> maximum
     container_candidates = filter(:remaining_depth => >=(layer_depth), db.containers)
@@ -122,102 +70,51 @@ function create_new_layer!(db::Database, container_id)
     return 0
 end
 
-"""
-Adds an item to the database.
-"""
 function add_item!(db::Database, dims::Vector{<:Real}, quantity::Int)
     id = new_id(db.items)
     push!(db.items, [id, dims..., quantity, false])
     return db.items[id,:]
 end
 
-"""
-Filters all the items that have remaining quantities greater than zero in the database.
-"""
-function filter_remaining_items(R)
-    return filter(row -> row[:quantity] > 0, R)
+function unopen_ranking_one(db::Database)
+    R = filter(:quantity => q -> q > 0, db.items[:,[:id, :quantity]])
+    R[:, :min_dim] = [minimum(row_dims) for row_dims in eachrow(db.items[:,[:dim1, :dim2, :dim3]])]
+    filter!(:min_dim => ==(maximum(R[:,:min_dim])), R)
+    return R
 end
 
-"""
-Filters all the items that are marked as open in the database.
-"""
-function filter_open_items(R)
-    return filter(row -> row[:open] == true, R)
+function unopen_ranking_two!(R)
+    filter!(:quantity => ==(maximum(R[:,:quantity])), R)
 end
 
-"""
-Filters all the items that have at least one dimension smaller than container depth `k`.
-"""
-function filter_fitting_depths(R, k)
-    dims = [minimum(row_dims) for row_dims in eachrow(db.items[:,[:dim1, :dim2, :dim3]])]
-    I = findall(<=(k), dims)
-    return R[I,:]
+function unopen_ranking_three!(db::Database, R)
+    candidates = innerjoin(db.items, R[:,Not(:quantity)], on=:id)
+    R[:, :max_dim] = [maximum(row_dims) for row_dims in eachrow(candidates[:,[:dim1,:dim2,:dim3]])]
 end
 
-"""
-Filters items whose smallest dimension is the greatest among all items.
-"""
-function filter_max_least_dim(R)
-    dims = [minimum(row_dims) for row_dims in eachrow(db.items[:,[:dim1, :dim2, :dim3]])]
-    I = findall(==(maximum(dims)), dims)
-    return R[I,:]
+function unopen_ranking(db::Database)
+    R = unopen_ranking_one(db)
+    unopen_ranking_two!(R)
+    unopen_ranking_three!(db, R)
+    return R[:,:id]
 end
 
-"""
-Filters items whose remaining quantity is the greatest among all items.
-"""
-function filter_greatest_quantity(R)
-    return filter(row -> row[:quantity] == maximum(R[:,:quantity]), R)
+function open_ranking(db::Database)
+    R = filter(:open => ==(true), db.items)
+    filter!(:quantity => ==(maximum(R[:,:quantity])), R)
+    return R[1,:id]
 end
 
-"""
-Filters items whose greatest dimension is the greatest among all items.
-"""
-function filter_max_greatest_dim(R)
-    dims = [maximum(row_dims) for row_dims in eachrow(R[:,[:dim1, :dim2, :dim3]])]
-    I = findall(==(maximum(dims)), dims)
-    return R[I,:]
-end
-
-function all_item_filters(R; open_only=false)
-    R = filter_remaining_items(R)
-    if open_only
-        R = filter_open_items(R)
-    end
-    return R |> filter_max_least_dim |> filter_greatest_quantity |> filter_max_greatest_dim
-end
-
-"""
-Checks if there are any items marked as open in the database.
-"""
 function any_items_open(db::Database)
     return any(==(true), db.items[:,:open])
 end
 
-"""
-Checks if there are any items with remaining quantity greater than zero in the database.
-"""
 function any_items_left(db::Database)
     return any(>(0), db.items[:,:quantity])
 end
 
-"""
-Selects a layer's primary item.
-"""
-function select_primary_item(db::Database; separate_rankings=true)
-    println("a")
-    R = db.items
-    if any_items_open(db)
-        if separate_rankings
-            return R |> filter_open_items |> filter_greatest_quantity |> first
-        end
-        all_item_filters(R, open_only=true)
-    end 
-    println("b")
-    R = all_item_filters(R)
-    println(R)
-    println(R |> first) 
-    return R |> first
+function select_primary_item(db::Database)
+    return any_items_open(db) ? open_ranking(db) : unopen_ranking(db) |> only
 end
 
 function add_placement!(db::Database, item_id, space_id, quantity, dims, coords)
@@ -336,7 +233,7 @@ function find_amalgamation_partner(db::Database, space_id)
     return partner
 end
 
-function amalgamate!(db::Database, space_id, partner_id, flexible_ratio)
+function amalgamate(db::Database, space_id, partner_id, flexible_ratio)
     space = db.spaces[space_id,:]
     space[:sibling] = new_id(db.spaces)
     space[:partner] = partner_id
@@ -365,7 +262,7 @@ function fill_column(db::Database, item, space_id, x)
     return item[:height] * quantity
 end
 
-function create_heightwise_space!!(db::Database, space_id, columns, item_width, col_height; last_column=false)
+function create_heightwise_space(db::Database, space_id, columns, item_width, col_height; last_column=false)
     space = db.spaces[space_id,:]
     width = item_width * (last_column ? 1 : columns) 
     height, depth = space[:height] - col_height, space[:depth]
@@ -379,7 +276,7 @@ function create_heightwise_space!!(db::Database, space_id, columns, item_width, 
     end
 end
 
-function create_widthwise_space!(db::Database, space_id, item_width, columns)
+function create_widthwise_space(db::Database, space_id, item_width, columns)
 
     space = db.spaces[space_id,:]
     width, height, depth = space[:width] - columns * item_width, space[:height], space[:depth]
@@ -390,7 +287,7 @@ function create_widthwise_space!(db::Database, space_id, item_width, columns)
     end
 end
 
-function create_depthwise_space!(db::Database, space_id, item_depth)
+function create_depthwise_space(db::Database, space_id, item_depth)
     space = db.spaces[space_id,:]
     width, height, depth = space[:width], space[:height], space[:depth] - item_depth
     x, y, z = space[:x], space[:y], space[:z] + item_depth
@@ -400,7 +297,7 @@ function create_depthwise_space!(db::Database, space_id, item_depth)
     end
 end
 
-function create_new_spaces!(db::Database, space_id, columns, item_width, item_depth, max_col_height, last_col_height)
+function create_new_spaces(db::Database, space_id, columns, item_width, item_depth, max_col_height, last_col_height)
     space = db.spaces[space_id,:]
     items = filter(:quantity => >(0), db.items)
     if isempty(items)
@@ -409,23 +306,23 @@ function create_new_spaces!(db::Database, space_id, columns, item_width, item_de
     min_dim = [minimum(items[:,:dim1]), minimum(items[:,:dim2]), minimum(items[:,:dim3])] |> minimum
     if columns > 1 && last_col_height < max_col_height
         if space[:height] - max_col_height >= min_dim
-            create_heightwise_space!!(db, space_id, columns - 1, item_width, max_col_height)
+            create_heightwise_space(db, space_id, columns - 1, item_width, max_col_height)
         end
         if space[:height] - last_col_height >= min_dim
-            create_heightwise_space!!(db, space_id, columns, item_width, last_col_height, last_column=true)
+            create_heightwise_space(db, space_id, columns, item_width, last_col_height, last_column=true)
         end
     elseif space[:height] - last_col_height >= min_dim
-        create_heightwise_space!!(db, space_id, columns, item_width, last_col_height)
+        create_heightwise_space(db, space_id, columns, item_width, last_col_height)
     end
     if space[:width] - item_width * columns >= min_dim
-        create_widthwise_space!(db, space_id, item_width, columns)
+        create_widthwise_space(db, space_id, item_width, columns)
     end
     if space[:depth] - item_depth > 0
-        create_depthwise_space!(db, space_id, item_depth)
+        create_depthwise_space(db, space_id, item_depth)
     end
 end
 
-function fill_space!(db::Database, item, space_id)
+function fill_space(db::Database, item, space_id)
     space = db.spaces[space_id,:]
     sibling = nothing
     if space[:type] != :amalgam && !iszero(space[:sibling])
@@ -448,30 +345,30 @@ function fill_space!(db::Database, item, space_id)
         columns += 1
         x += item[:width]
     end
-    create_new_spaces!(db, space_id, columns, item[:width], item[:depth], max_col_height, last_col_height)
+    create_new_spaces(db, space_id, columns, item[:width], item[:depth], max_col_height, last_col_height)
     space[:status] = :filled
 end
 
-function wall_building!(db::Database; flexible_ratio=.0)
-    create_new_layer!(db)
+function wall_building(db::Database; flexible_ratio=.0)
+    create_new_layer(db)
     i = 1
     while i <= nrow(db.spaces)
         space = db.spaces[i,:]
         partner = find_amalgamation_partner(db, space[:id])
         if !isnothing(partner)
-            amalgamate!(db, space[:id], partner[:id], flexible_ratio)
+            amalgamate(db, space[:id], partner[:id], flexible_ratio)
         end
         item = select_item_for_space(db, space[:id])
         if isnothing(item)
             space[:status] = :rejected
         else
-            fill_space!(db, item, space[:id])
+            fill_space(db, item, space[:id])
         end
         if i == nrow(db.spaces)
             if !any_items_left(db)
                 return
             end
-            create_new_layer!(db)
+            create_new_layer(db)
         end
         i += 1
     end
@@ -547,11 +444,11 @@ add_item!(db, [50, 60, 40], 1383)
 add_container!(db, B)
 add_container!(db, A)
 add_container!(db, A)
-wall_building!(db, flexible_ratio=.1)
+wall_building(db, flexible_ratio=.1)
 
 # db = Database()
 # add_item!(db, [5, 7, 10], 50)
 # add_item!(db, [8, 3, 7], 70)
 # add_item!(db, [3, 4, 5], 110)
 # add_container!(db, [40, 50, 100])
-# wall_building!(db, flexible_ratio=0)
+# wall_building(db, flexible_ratio=0)
