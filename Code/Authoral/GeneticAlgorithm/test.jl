77ef92b5fa0a51702406f3482b6b38a2d4f4bca8
phcentenaro7 <phc7blu@gmail.com>
2024-08-15 20:05:54 -0300

    report updated

--- Code/Authoral/GeneticAlgorithm/test.jl
+++ Code/Authoral/GeneticAlgorithm/test.jl
@@ -2,14 +2,10 @@ include("ga.jl")
 import .GeneticAlgorithm as ga
 using Gurobi
 
-ga.logging_on()
-db = ga.Database()
-ga.add_item!(db, [40, 40, 50], false, 2000)
-ga.add_item!(db, [27, 43, 32], false, 2000)
-ga.add_item!(db, [50, 60, 40], false, 2000)
-den = 260*440*1860
-ga.add_container!(db, [220,350,720], 220*350*720/den)
-ga.add_container!(db, [260,440,1000], 260*440*1000/den)
-ga.add_container!(db, [260,440,1860], 260*440*1860/den)
-pop, pop_fit, fit_plot = ga.solve_CLP(db, 10, Gurobi.Optimizer, plot_data=true, pop_ratio=5)
-fit_plot
+db = ga.Database()  
+ga.add_item!(db, [40, 40, 50], true, 122)
+ga.add_item!(db, [27, 43, 32], true, 124)
+ga.add_item!(db, [50, 60, 40], true, 1383)
+ga.add_container!(db, [260,440,1400], 260*440*1400)
+pop_fitness = ga.solve_CLP(db, 10)
+println(pop_fitness)
